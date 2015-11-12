160571ef2cf35e2f7711f305b0cfbd1f7263d55a
djhcooh <innuendoooooo@gmail.com>
2015-11-12 16:33:10 +0900

    add way of thinking

--- Domemo.scala
+++ Domemo.scala
@@ -3,17 +3,21 @@ import scala.util.Random.shuffle
 import ren.kujoka.common.Reader.readIntLoop
 import scala.io.StdIn.readLine
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Queue
 
 object Domemo {
-  private var cards: Seq[Int] = _ 
+  private var cards: Queue[Int] = new Queue[Int]
   private var openCards: Seq[Int] = _
   private var players: Seq[Player] = _
+  private var logs: Queue[Int] = new Queue[Int]
+
   def main(args:Array[String]) {
     var yn = ""
     do {
-      cards = Seq.empty[Int]
+      cards = new Queue[Int]
       openCards = Seq.fill(7)(0)
       players = Seq.empty[Player]
+      logs = new Queue[Int]
       game()
       var end = false
       do {
@@ -43,44 +47,60 @@ object Domemo {
           "Please enter the correct value", 1, 7)
       gameEnd = answerCheck(0, ans)
       for (i <- 1 until nop; if gameEnd == false) {
-        gameEnd = answerCheck(i, players(i).think(players:_*)(openCards:_*))
+        gameEnd = answerCheck(i, players(i).think(players:_*)(openCards:_*)(logs:_*))
       }
     } while (gameEnd == false)
   }
 
   def cardsSet() {
     for (i <- 0 to 6) {
-      for (j <- 0 to i) cards = cards :+ (i + 1)
+      for (j <- 0 to i) cards += (i + 1)
     }
     cards = shuffle(cards) 
   }
 
   def playersSet(nop:Int) {
-    for (i <- 0 to nop) players = players :+ new Player(i)
+    players = players :+ new Player(0)
+    for (i <- 1 to nop - 1) {
+      val str = if (i == 1) 
+      """|Please select the nature of the computer
+         |0:Accurate
+         |1:Accurate(but remark ignored)
+         |2:Suitable(but not weak))
+         |(not much different)
+         | > """.stripMargin else " > "
+      val cpselect =
+        readIntLoop(str, "Please enter the correct value", 0, 2)
+      players =
+        if (cpselect == 0) players :+ new AccurateComputer(i) 
+        else if (cpselect == 1) players :+ new Player(i)
+        else players :+ new RandomComputer(i)
+    }
+    players = players :+ new Player(nop)
     for (player <- players) {
       for (_ <- 1 to 6 / (nop / 2) + 1) {
-        player.handsPlus(cards.head)
-        cards = cards.drop(1)
+        player.handsPlus(cards.dequeue)
       }
     }
     if (nop == 4) {
       for (i <- 0 to 3) {
-        players(i).handsPlus(cards.head)
-        cards = cards.drop(1)
+        players(i).handsPlus(cards.dequeue)
       }
     }
   }
 
   def answerCheck(pNum:Int, num:Int):Boolean = {
+    if (logs.length >= players.length - 2) logs.dequeue
     var handsTemp = ListBuffer.empty[Int]
     handsTemp = players(pNum).hands.to[ListBuffer]
     if (handsTemp.indexWhere(n => n == num) != -1) {
-      println("Player " + pNum + "'s answer = " + num + " is exist")
+      println(players(pNum).playerName + " answer = " + num + " is exist")
       handsTemp.remove(handsTemp.indexWhere(n => n == num))
       players(pNum).hands = handsTemp.toSeq
       openCardsAdd(num)
     } else {
-      println("Player " + pNum + "'s answer = " + num + " is not exist")
+      logs += num
+      println(players(pNum).playerName + " answer = " + num + " is not exist")
       players(pNum).notExist(num)
     }
     if (players(pNum).hands.length != 0) {
--- Player.scala
+++ Player.scala
@@ -1,11 +1,13 @@
 package ren.kujoka.Domemo
+import scala.util.Random.shuffle
+class Player(protected val playerNum: Int) {
+  protected var hands_ = Seq.empty[Int]
+  protected var exist = Seq.fill(7)(true)
+  protected val playerName_ =
+    if (playerNum == 0) "Your" else playerNum + 1 + "P's"
 
-class Player(private val playerNum: Int) {
-  private var hands_ = Seq.empty[Int]
-  private var exist = Seq.fill(7)(true)
-  
   def show() {
-    print("\n" + playerNum + ":")
+    print("\n" + (playerNum + 1) + "P:")
     for (hand <- hands)
       print(hand + " ")
   }
@@ -18,7 +20,7 @@ class Player(private val playerNum: Int) {
     exist = exist.updated(num - 1, false)
   }
 
-  def think(others: Player*)(openCards: Int*): Int = {
+  def think(others: Player*)(openCards: Int*)(logs: Int*): Int = {
     var nums: Seq[Double] = openCards.map(_.toDouble)
     for (i <- 0 until others.length - 1 if i != playerNum) {
       for (otherHand <- others(i).hands)
@@ -35,4 +37,48 @@ class Player(private val playerNum: Int) {
   def hands: Seq[Int] = hands_
 
   def hands_=(newHands: Seq[Int]) = hands_ = newHands
+
+  def playerName: String = playerName_
+}
+
+class RandomComputer(playerNum: Int) extends Player(playerNum) {
+  override def think(others: Player*)(openCards: Int*)(logs: Int*): Int = {
+    var nums: Seq[Double] = openCards.map(_.toDouble)
+    for (i <- 0 until others.length - 1 if i != playerNum) {
+      for (otherHand <- others(i).hands)
+        nums = nums.updated(otherHand - 1, nums(otherHand - 1) + 1)
+    }
+    var ans = Seq.empty[Int] 
+    for (i <- 0 to 6 if nums(i) != (i + 1); if exist(i) == true) {
+      ans = ans :+ (i + 1)
+    }
+    ans = shuffle(ans)
+    ans.head
+  } 
+}
+
+class AccurateComputer(playerNum: Int) extends Player(playerNum) {
+  override def think(others: Player*)(openCards: Int*)(logs: Int*): Int = {
+    var nums: Seq[Double] = openCards.map(_.toDouble)
+    for (i <- 0 until others.length - 1 if i != playerNum) {
+      for (otherHand <- others(i).hands)
+        nums = nums.updated(otherHand - 1, nums(otherHand - 1) + 1)
+    }
+    for (log <- logs) {
+      if (exist(log - 1) == true) {
+        if (nums(log - 1) == log - 1) {
+          println(log +" "+ exist(log - 1))
+          exist = exist.updated(log - 1, false)
+        } else if (nums(log - 1) == log - 2) {
+          nums = nums.updated(log - 1, nums(log - 1) + 1)
+        }
+      }
+    }
+    var pos: Double = 0
+    for (i <- 0 to 6) {
+      pos = if (exist(i) == true) ((i + 1 - nums(i)) * (i + 1)) / 28 else 0
+      nums = nums.updated(i, pos)
+    }
+    nums.indexWhere(_ == nums.max) + 1
+  }
 }
